1a351b8ef2e0dd290935c01f65abf481c01f951c
Zaitam <gotzmatiascode@gmail.com>
2022-06-06 18:01:32 -0300

    Edited HTML and CSS

--- layouts/_default/index.js
+++ layouts/_default/index.js
@@ -1,5 +1,5 @@
 var docs = [
-{{ range $index, $page := (where .Site.Pages "Section" "docs") -}}
+{{ range $index, $page := (where .Site.Pages "Section" "docs") -}},
   {
     id: {{ $index }},
     title: "{{ .Title }}",
